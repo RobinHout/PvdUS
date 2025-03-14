a65657dc7778f99e367b9a43827c77ba4dc20727
Robin <21104964@student.hhs.nl>
2025-03-14 19:31:25 +0100

    Routing gefixt

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Nodig omdat Next.js standaard een image optimizer gebruikt
   },
-  basePath: "/PvdUS", // Vervang dit met de naam van je GitHub repository
-  assetPrefix: "/PvdUS/", // Nodig voor statische assets
 };
 
 module.exports = nextConfig;
