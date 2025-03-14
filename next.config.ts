3d1bab0b093f99f9283dddb5028d12c98a4d8102
Robin <21104964@student.hhs.nl>
2025-03-14 18:41:21 +0100

    GH Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Nodig omdat Next.js standaard een image optimizer gebruikt
+  },
+  basePath: "/PvdUS", // Vervang dit met de naam van je GitHub repository
+  assetPrefix: "/PvdUS/", // Nodig voor statische assets
 };
 
-export default nextConfig;
+module.exports = nextConfig;
