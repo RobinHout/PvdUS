33d2546833c1b3112d7104b9eb7fc5af2b9bb0cc
Robin <21104964@student.hhs.nl>
2025-04-27 22:13:35 +0200

    Nieuwe dropdown toegevoegd

---
+++ src/app/Components/dropdown.tsx
@@ -0,0 +1,94 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+const dropdown = () => {
+    return (
+        <Menu as="div" className="relative inline-block text-left">
+            <div>
+                <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 text-3xl py-2font-semibold text-[#f280cb] hover:text-gray-100 focus:outline-hidden">
+                    <i className="fa fa-bars" id="bars"></i>
+                </MenuButton>
+            </div>
+
+            <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+            >
+                <div className="py-1">
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Homepagina
+                        </a>
+                    </MenuItem>
+                </div>
+                <div className="py-1">
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Fractie 2025-2026
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Fractie 2024-2025
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Oude fracties
+                        </a>
+                    </MenuItem>
+                </div>
+                <div className="py-1">
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Speerpunten & initiatieven
+                        </a>
+                    </MenuItem>
+                </div>
+                <div className="py-1">
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Universiteitsraad
+                        </a>
+                    </MenuItem>
+                </div>
+                <div className="py-1">
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Contact
+                        </a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                        >
+                            Word lid!
+                        </a>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </Menu>
+    );
+};
+export default dropdown;
--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -5,113 +5,33 @@ import "./headerStyle.css";
 import logo from "./Img/logo-pvdus.png";
 import Image from "next/image";
 import Link from "next/link";
+import Dropdown from "./dropdown";
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false);
-    // const [hideHeader, setHideHeader] = useState(false);
-
-    // const pathname = usePathname();
-    // const isHomePage = pathname === "/";
-
-    // const handleScroll = () => {
-    //     if (window.scrollY > 50 && window.scrollY < 2000) {
-    //         setScrolled(true);
-    //     } else {
-    //         setScrolled(false);
-    //     }
-    // };
-    // useEffect(() => {
-    //     window.addEventListener("scroll", handleScroll);
-    //     return () => window.removeEventListener("scroll", handleScroll);
-    // }, []);
-
     useEffect(() => {
         const handleScroll = () => {
             const scrollY = window.scrollY;
-            // const windowBottom = window.innerHeight + scrollY;
-            // const pageHeight = document.body.offsetHeight;
-
-            // Set scrolled flag
             if (scrollY > 50) {
                 setScrolled(true);
             } else {
                 setScrolled(false);
             }
-
-            // Hide header at the bottom
-            // if (isHomePage) {
-            //     if (windowBottom >= pageHeight) {
-            //         setHideHeader(true);
-            //         console.log(windowBottom + "sdf" + pageHeight);
-            //     } else {
-            //         setHideHeader(false);
-            //     }
-            // }
         };
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
-    // if (hideHeader) return null;
     return (
         <>
-            <header
-                // id={scrolled ? "scrolled" : isHomePage ? "headerNiks" : ""}
-                id={scrolled ? "scrolled" : ""}
-                className="header"
-            >
+            <header id={scrolled ? "scrolled" : ""} className="header">
                 <Link className="logo" href="/">
                     <Image src={logo} alt="Logo van PvdUS" priority />
                 </Link>
                 <a href="home">
                     <h1 className="titel">Partij voor de Utrechtse Student</h1>
                 </a>
-                <div>
-                    <a
-                        href=""
-                        className="knop"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setMenuOpen(!menuOpen);
-                        }}
-                        aria-label="menuButton"
-                    >
-                        <i className="fa fa-bars" id="bars"></i>
-                    </a>
-                </div>
-                <nav id="menu" className={menuOpen ? "openMenu" : "menu"}>
-                    <ul>
-                        <li>
-                            <Link href="/">Homepagina</Link>
-                        </li>
-                        <li>
-                            <Link href="/NieuweFractie">Fractie 2025-2026</Link>
-                        </li>
-                        <li>
-                            <Link href="/Fractie">Fractie 2024-2025</Link>
-                        </li>
-                        <li>
-                            <Link href="/OudeFracties">Oude fracties</Link>
-                        </li>
-                        <li>
-                            <Link href="/Speerpunten">
-                                Speerpunten & initiatieven
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/Universiteitsraad">
-                                Universiteitsraad
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/Contact">Contact</Link>
-                        </li>
-                        <li>
-                            <Link href="/WordLid">Word lid!</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <Dropdown />
             </header>
         </>
     );
