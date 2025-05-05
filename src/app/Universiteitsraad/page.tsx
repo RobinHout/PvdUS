528e71e07aad856347be0b07ca719fc9c2502f95
Robin <21104964@student.hhs.nl>
2025-05-05 11:51:06 +0200

    Universiteitsraad begin

--- src/app/Universiteitsraad/page.tsx
+++ src/app/Universiteitsraad/page.tsx
@@ -1,6 +1,6 @@
 import TekstBlok from "../Components/tekstBlok";
 // import "..Speerpunten/speerpuntenStyle.css";
-export default function Speerpunten() {
+export default function Universiteitsraad() {
     return (
         <>
             <TekstBlok titel="Universiteitsraad" tekst="" />
@@ -59,7 +59,21 @@ Ten slotte heeft de Uraad het recht om ongevraagd advies te geven en hebben we t
                         </h2>
                         <p className="mb-6">
                             De Universiteitsraad (U-raad, UR) is de centrale
-                            medezeggenschapsraad van de Universiteit Utrecht...
+                            medezeggenschapsraad van de Universiteit Utrecht. De
+                            U-raad vertegenwoordigt de belangen van alle
+                            studenten en medewerkers en bestaat uit twaalf
+                            personeelsleden (ieder voor acht uur per week) en
+                            twaalf studentleden (ieder voor 32 uur per week).
+                            Als studentleden vertegenwoordigen wij de belangen
+                            van alle 40.000 studenten aan de UU op het hoogste
+                            niveau. De U-raad vergadert met het College van
+                            Bestuur (CvB) over hun beleidsplannen en oefent op
+                            deze manier invloed uit. De Universiteit Utrecht
+                            heeft twee bestuurslagen: het universiteitsbrede
+                            niveau en het niveau van de diensten en faculteiten.
+                            De U-raad is het centrale formele
+                            medezeggenschapsorgaan van de UU, zoals te zien in
+                            het organogram hieronder.
                         </p>
 
                         <h3 className="text-2xl font-semibold mb-3">
@@ -67,7 +81,9 @@ Ten slotte heeft de Uraad het recht om ongevraagd advies te geven en hebben we t
                         </h3>
                         <p className="mb-6">
                             Het CvB is belast met het bestuur van de
-                            universiteit...
+                            universiteit. De leden worden benoemd door de Raad
+                            van Toezicht. Voordat de leden van het CvB worden
+                            benoemd, wordt de U-raad hierover gehoord.
                         </p>
 
                         <h3 className="text-2xl font-semibold mb-3">
@@ -75,40 +91,84 @@ Ten slotte heeft de Uraad het recht om ongevraagd advies te geven en hebben we t
                         </h3>
                         <p className="mb-6">
                             Op het niveau van de faculteiten bestaat ook
-                            medezeggenschap...
+                            medezeggenschap, maar dan in de vorm van
+                            faculteitsraden. Waar de universiteitsraad met het
+                            CvB vergadert, vergadert de faculteitsraad met de
+                            decaan van de betreffende faculteit. Gedurende het
+                            jaar onderhouden wij contact met de faculteiten, om
+                            op die manier op de hoogte te blijven van zaken die
+                            op decentraal niveau spelen.
                         </p>
 
                         <h3 className="text-2xl font-semibold mb-3">
                             De Raad van Toezicht
                         </h3>
                         <p className="mb-6">
-                            De Raad van Toezicht (RvT) is het toezichthoudend
-                            orgaan van de UU...
+                            De Raad van Toezicht (RvT) is, zoals de naam al
+                            zegt, het toezichthoudend orgaan van de UU. Leden
+                            worden door de minister van OCW benoemd. Voor
+                            onderwerpen, zoals het Strategisch Plan, heeft het
+                            CvB goedkeuring van de RvT nodig.
                         </p>
 
                         <h2 className="text-3xl font-bold mt-10 mb-4">
                             Onze Bevoegdheden
                         </h2>
+                        <p className="mb-6">
+                            De universiteitsraad kan drie verschillende rechten
+                            uitoefenen op de stukken die besproken worden;
+                            instemmingsrecht, adviesrecht & initiatiefrecht.
+                        </p>
 
                         <h3 className="text-2xl font-semibold mb-3">
                             Instemmingsrecht
                         </h3>
                         <p className="mb-6">
-                            Op sommige stukken heeft de raad instemmingsrecht...
+                            Op sommige stukken heeft de raad instemmingsrecht,
+                            wat betekent dat een besluit pas definitief kan
+                            worden genomen nadat de U-raad heeft ingestemd. De
+                            raad heeft onder andere instemmingsrecht op de
+                            volgende onderwerpen: Het strategisch plan; Het
+                            model Onderwijs- en Examenreglement; Het bestuurs-
+                            en beheersreglement; Het model faculteitsreglement;
+                            Hoofdlijnen van de begroting.
                         </p>
 
                         <h3 className="text-2xl font-semibold mb-3">
                             Adviesrecht
                         </h3>
                         <p className="mb-6">
-                            Soms heeft de U-raad enkel adviesrecht...
+                            Echter, soms heeft de U-raad enkel adviesrecht, op
+                            grond waarvan het advies mag geven op een stuk. Het
+                            CvB moet de raad altijd van repliek voorzien bij
+                            advies. De raad heeft bijvoorbeeld adviesrecht op:
+                            De kaderbrief; De begroting; Het beleid ten aanzien
+                            van het instellingscollegegeld.
                         </p>
 
                         <h3 className="text-2xl font-semibold mb-3">
                             Initiatiefrecht
                         </h3>
                         <p className="mb-6">
-                            De Universiteitsraad heeft tevens initiatiefrecht...
+                            De Universiteitsraad heeft tevens initiatiefrecht.
+                            Dit betekent dat je als U-raadslid de mogelijkheid
+                            krijgt om zelf nieuw beleid voor te stellen voor de
+                            UU, iets te verbeteren of iets aan te passen. Een
+                            initiatief leidt meestal tot een nota; een
+                            geschreven advies/voorstel aan het CvB. Het CvB zal
+                            deze nota vervolgens in behandeling nemen en
+                            bespreken in de vergadercyclus.
+                        </p>
+                        <p className="mb-6">
+                            Als U-raadslid houdt je je dus zowel bezig met het
+                            kritisch beoordelen en controleren van de
+                            beleidsplannen van het CvB, als met je eigen
+                            initiatieven om de UU te verbeteren. Klik HIER voor
+                            een aantal van onze initiatieven uit de afgelopen
+                            jaren. Ten slotte heeft de Uraad het recht om
+                            ongevraagd advies te geven en hebben we te allen
+                            tijde informatierecht; het recht om alle informatie
+                            te krijgen.
                         </p>
                     </div>
                 </div>
